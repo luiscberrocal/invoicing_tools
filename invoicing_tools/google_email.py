cd475500e16c810a6fcbe0c2139ede931388689f
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2022-06-15 06:20:50 -0500

    Google gmail api not working.

--- invoicing_tools/google_email.py
+++ invoicing_tools/google_email.py
@@ -1,7 +1,51 @@
-import os
 import base64
+import os
+import pickle
 from typing import List
-import time
+
+from google.auth.transport.requests import Request
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+
+
+def create_service(client_secret_file, api_name, api_version, *scopes, prefix=''):
+    CLIENT_SECRET_FILE = client_secret_file
+    API_SERVICE_NAME = api_name
+    API_VERSION = api_version
+    SCOPES = [scope for scope in scopes[0]]
+
+    cred = None
+    working_dir = os.getcwd()
+    token_dir = 'token files'
+    pickle_file = f'token_{API_SERVICE_NAME}_{API_VERSION}{prefix}.pickle'
+
+    ### Check if token dir exists first, if not, create the folder
+    if not os.path.exists(os.path.join(working_dir, token_dir)):
+        os.mkdir(os.path.join(working_dir, token_dir))
+
+    if os.path.exists(os.path.join(working_dir, token_dir, pickle_file)):
+        with open(os.path.join(working_dir, token_dir, pickle_file), 'rb') as token:
+            cred = pickle.load(token)
+
+    if not cred or not cred.valid:
+        if cred and cred.expired and cred.refresh_token:
+            cred.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRET_FILE, SCOPES)
+            cred = flow.run_local_server()
+
+        with open(os.path.join(working_dir, token_dir, pickle_file), 'wb') as token:
+            pickle.dump(cred, token)
+
+    try:
+        service = build(API_SERVICE_NAME, API_VERSION, credentials=cred)
+        print(API_SERVICE_NAME, API_VERSION, 'service created successfully')
+        return service
+    except Exception as e:
+        print(e)
+        print(f'Failed to create service instance for {API_SERVICE_NAME}')
+        os.remove(os.path.join(working_dir, token_dir, pickle_file))
+        return None
 
 class GmailException(Exception):
     """gmail base exception class"""
@@ -11,11 +55,12 @@ class NoEmailFound(GmailException):
     """no email found"""
 
 
-def search_emails(query_stirng: str, label_ids: List = None):
+def search_emails(query_string: str, label_ids: List = None, max_results: int = 50):
     try:
         message_list_response = service.users().messages().list(
             userId='me',
             labelIds=label_ids,
+            maxResults=max_results,
             q=query_string
         ).execute()
 
@@ -26,6 +71,7 @@ def search_emails(query_stirng: str, label_ids: List = None):
             message_list_response = service.users().messages().list(
                 userId='me',
                 labelIds=label_ids,
+                maxResults=max_results,
                 q=query_string,
                 pageToken=next_page_token
             ).execute()
@@ -34,6 +80,7 @@ def search_emails(query_stirng: str, label_ids: List = None):
             next_page_token = message_list_response.get('nextPageToken')
         return message_items
     except Exception as e:
+        print(f'{e}')
         raise NoEmailFound('No emails returned')
 
 
@@ -59,7 +106,7 @@ def get_message_detail(message_id, msg_format='metadata', metadata_headers: List
 
 
 if __name__ == '__main__':
-    CLIENT_FILE = 'client-secret.json'
+    CLIENT_FILE = '../output/client_secret_gmail.json'
     API_NAME = 'gmail'
     API_VERSION = 'v1'
     SCOPES = ['https://mail.google.com/']
@@ -68,21 +115,22 @@ if __name__ == '__main__':
     query_string = 'has:attachment'
 
     save_location = os.getcwd()
-    email_messages = search_emails(query_string)
+    email_messages = search_emails(query_string, max_results=5)
 
     for email_message in email_messages:
         messageDetail = get_message_detail(email_message['id'], msg_format='full', metadata_headers=['parts'])
-        messageDetailPayload = messageDetail.get('payload')
+        print(f'{messageDetail =}')
+        # messageDetailPayload = messageDetail.get('payload')
 
-        if 'parts' in messageDetailPayload:
-            for msgPayload in messageDetailPayload['parts']:
-                file_name = msgPayload['filename']
-                body = msgPayload['body']
-                if 'attachmentId' in body:
-                    attachment_id = body['attachmentId']
-                    attachment_content = get_file_data(email_message['id'], attachment_id, file_name, save_location)
+        # if 'parts' in messageDetailPayload:
+        #     for msgPayload in messageDetailPayload['parts']:
+        #         file_name = msgPayload['filename']
+        #         body = msgPayload['body']
+        #         if 'attachmentId' in body:
+        #             attachment_id = body['attachmentId']
+        #             attachment_content = get_file_data(email_message['id'], attachment_id, file_name, save_location)
 
-                    with open(os.path.join(save_location, file_name), 'wb') as _f:
-                        _f.write(attachment_content)
-                        print(f'File {file_name} is saved at {save_location}')
-        time.sleep(0.5)
+        #             with open(os.path.join(save_location, file_name), 'wb') as _f:
+        #                 _f.write(attachment_content)
+        #                 print(f'File {file_name} is saved at {save_location}')
+        # time.sleep(0.5)
