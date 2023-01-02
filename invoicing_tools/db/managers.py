a2a624661af1281c434c94a6253ee008548c3cd1
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2023-01-02 11:16:01 -0500

    Database working with json.

--- invoicing_tools/db/managers.py
+++ invoicing_tools/db/managers.py
@@ -1,11 +1,26 @@
 import json
+from datetime import datetime
 from pathlib import Path
 
-from invoicing_tools.models import JurisPerson
+from pydantic import BaseModel
+
+from invoicing_tools.models import JurisPerson, FiscalInvoice
+
+
+class ModelEncoder(json.JSONEncoder):
+
+    def default(self, obj):
+        if isinstance(obj, BaseModel):
+            return obj.dict()
+        elif isinstance(obj, datetime):
+            return obj.strftime('%Y-%m-%d %H:%M')
+        else:
+            return json.JSONEncoder.default(obj)
 
 
 class JSONDatabase:
     PERSON_TABLE = 'persons'
+    INVOICE_TABLE = 'invoices'
 
     def __init__(self, db_file: Path):
         self.db_file = db_file
@@ -19,9 +34,18 @@ class JSONDatabase:
         if person_in_db is None:
             self.database[self.PERSON_TABLE][person.ruc] = person
         else:
-            raise Exception(f'Person with ruc {person.ruc} already exists {person.company}')
+            raise Exception(f'Person with ruc {person.ruc} already exists {person.name}')
+
+    def add_fiscal_invoice(self, invoice: FiscalInvoice):
+        if not self.database.get(self.INVOICE_TABLE):
+            self.database[self.INVOICE_TABLE] = dict()
+
+        in_db = self.database[self.INVOICE_TABLE].get(invoice.number)
+        if in_db is None:
+            self.database[self.INVOICE_TABLE][invoice.number] = invoice
+        else:
+            raise Exception(f'There is an existing invoice with number {invoice.number}')
 
     def save(self):
         with open(self.db_file, 'w') as json_file:
-            json.dump(self.database, json_file, default=lambda x: x.dict())
-
+            json.dump(self.database, json_file, cls=ModelEncoder)
--- tests/db/test_managers.py
+++ tests/db/test_managers.py
@@ -1,5 +1,12 @@
+import json
+from datetime import datetime
+
+from pydantic import BaseModel
+
 from invoicing_tools.db.managers import JSONDatabase
-from invoicing_tools.models import JurisPerson
+from invoicing_tools.models import JurisPerson, FiscalInvoice
+
+
 
 
 class TestJSONDatabase:
@@ -9,5 +16,14 @@ class TestJSONDatabase:
         person = JurisPerson(short_name='CMMA', name='Super company', ruc='855-888-888', dv='66')
         database = JSONDatabase(db_file)
         database.add_person(person)
+        database.save()
+        assert db_file.exists()
+
+    def test_add_invoice(self, output_folder):
+        db_file = output_folder / '_json_db.json'
+        invoice = FiscalInvoice(number=1, date=datetime.now(), ruc='555-888-999',
+                                company='Super company', amount=200.25)
+        database = JSONDatabase(db_file)
+        database.add_fiscal_invoice(invoice)
         database.save()
         assert db_file.exists()
