0ae85297211e91dbb38459df9f136e91ae91903f
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2023-03-19 15:01:42 -0500

    Bump version: 0.3.2 → 0.3.3

--- invoicing_tools/__init__.py
+++ invoicing_tools/__init__.py
@@ -3,7 +3,7 @@
 
 __author__ = """Luis Carlos Berrocal"""
 __email__ = 'luis.berrocal.1942@gmail.com'
-__version__ = '0.3.2'
+__version__ = '0.3.3'
 
 from invoicing_tools.config.configuration import ConfigurationManager
 
