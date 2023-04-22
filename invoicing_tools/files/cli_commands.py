6473a1c27b0cdcf071a17aad632b7fb991f02cc1
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2023-04-22 08:11:39 -0500

    Renaming cli command

--- invoicing_tools/files/cli_commands.py
+++ invoicing_tools/files/cli_commands.py
@@ -1,6 +1,31 @@
+import os
+import re
+import webbrowser
 from pathlib import Path
+from typing import List
+
+import click
 
 
+@click.command()
+@click.option('-d', '--directory', help='Folder with the scanned invoices.', type=click.Path(exists=True))
+def rename(directory: Path):
+    pattern = re.compile(r'Scanned_\d{8}-\d{4}\.(pdf|PDF)')
+    files_to_rename: List[Path] = []
 
-def rename(directory:Path):
-    pass
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            if pattern.match(file):
+                files_to_rename.append(Path(root) / file)
+                # click.secho(f'{file}', fg='blue')
+    for idx, file in enumerate(files_to_rename, 1):
+        click.secho(f'{idx} {file.name}', fg='yellow')
+    file_num = click.prompt('Select file to rename')
+    file_to_rename = files_to_rename[int(file_num) - 1]
+    webbrowser.open_new_tab(str(file_to_rename))
+
+    invoice_number = click.prompt('Invoice number')
+    amount = click.prompt('Amount')
+    date_invoiced = click.prompt('Date')
+    client_short_name = click.prompt('Client short name')
+
--- invoicing_tools/naming.py
+++ invoicing_tools/naming.py
@@ -6,11 +6,24 @@ from invoicing_tools.models import FiscalInvoice
 
 
 def rename_fiscal_invoice(invoice: FiscalInvoice, database: JSONDatabase, pdf_file: Path, target_folder: Path,
-                          prefix: str = 'FFiscal'):
-    sep = '-'
+                          prefix: str = 'FFiscal') -> Path:
     company = database.get_person(invoice.ruc)
-    target_filename = f'{prefix}{sep}{company.short_name}{sep}{invoice.number:04}{sep}{pdf_file.suffix}'
+    short_name = company.short_name
+    invoice_number = invoice.number
+    target_file = rename_fiscal_invoice_with_short_name(short_name=short_name,
+                                                        invoice_number=invoice_number, pdf_file=pdf_file,
+                                                        target_folder=target_folder,
+                                                        prefix=prefix)
+    return target_file
+
+
+def rename_fiscal_invoice_with_short_name(short_name: str, invoice_number: int, pdf_file: Path,
+                                          target_folder: Path, prefix: str = 'FFiscal',
+                                          delete_original: bool = True) -> Path:
+    sep = '-'
+    target_filename = f'{prefix}{sep}{short_name}{sep}{invoice_number:04}{sep}{pdf_file.suffix}'
     full_target_file = target_folder / target_filename
     shutil.copy(pdf_file, full_target_file)
-
-
+    if delete_original:
+        pdf_file.unlink()
+    return full_target_file
